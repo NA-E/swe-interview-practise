4a93f703d71ed218d7106ef1005ca6a200e92975
Nourin-Eka <eka.nourin@gmail.com>
2019-10-28 23:10:57 +0600

    Started Tree

---
+++ LeetCode/Tree/94. Binary Tree Inorder Traversal.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<> ();
+        
+        List<Integer> result = new ArrayList<> ();
+        
+        while(root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            
+            root = stack.pop();
+            result.add(root.val);
+            root = root.right;
+            
+        }
+        
+        return result;
+    }
+}
